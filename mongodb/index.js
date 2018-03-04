c948897a2435a93a8aa5f4bf3ac216713b0cc527
Kananek T <info.dvgamer@gmail.com>
2018-03-04 16:57:45 +0700

    fixed name and connection

--- mongodb/index.js
+++ mongodb/index.js
@@ -6,20 +6,21 @@ const { debug } = require('../helper/variables')
 
 let db = []
 let mongodb = {
-  dbOpen: options => {
+  MongooseOpen: async options => {
     // { user: 'admin', pass: 'admin', dbname: 'test' }
     const MONGODB = process.env.MONGODB || '127.0.0.1:27017'
     const login = { user: options.user, pass: options.pass }
-    const conn = `mongodb://${MONGODB}/${options.dbname}${options.user ? '?authMode=scram-sha1?authSource=admin' : ''}`
-    if (debug) console.log(`[MongoDB] Connecting... 'mongodb://${MONGODB}/${options.dbname}'`)
-    return mongoose.connect(conn, login)
+    const dbo = `mongodb://${MONGODB}/${options.dbname}${options.user ? '?authMode=scram-sha1?authSource=admin' : ''}`
+    if (debug) console.log(`[MongoDB] ${mongoose.connection.readyState}:Connecting... 'mongodb://${MONGODB}/${options.dbname}'`)
+    await mongoose.connect(dbo, login)
   },
   tests: mongoose.model('db-tests', mongoose.Schema({ any: mongoose.Schema.Types.Mixed }), 'db-tests'),
-  dbClose: () => {
+  MongooseClose: async () => {
+    await mongoose.connection.close()
     if (debug) console.log(`[MongoDB] Close connection.`)
-    return mongoose.connection.close()
   }
 }
+
 db = db.concat(nicehash)
 
 for (var i = 0; i < db.length; i++) {
