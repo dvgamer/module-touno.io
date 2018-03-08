cfe23601e76c47e7fa234f4a6dd6b94583ba6876
Kananek T <info.dvgamer@gmail.com>
2018-03-09 00:17:02 +0700

    fixed modules

--- notify/index.js
+++ notify/index.js
@@ -4,16 +4,14 @@ const request = require('request-promise')
 const apiMinerLine = process.env.LINE_MINER_API
 const apiTounoLine = process.env.LINE_TOUNO_API
 const apiDevopsLine = process.env.LINE_DEVOPS_API
-const LINEID = process.env.LINE_ID
-
-if (!LINEID) throw new Error('LINEID not found.')
+const apiLineID = process.env.LINE_ID
 
 const nameTouno = `https://hooks.slack.com/services/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`
 const nameException = `https://hooks.slack.com/services/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`
 const nameNotify = `https://hooks.slack.com/services/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`
 const client = (token, msg) => {
   const cb = new api.Client({ channelAccessToken: token })
-  return cb.pushMessage(LINEID, msg)
+  return cb.pushMessage(apiLineID, msg)
 }
 
 const webhook = (uri, name, message, channel) => {
