734afab57e3759729f85865c9fa75714386a93c3
Kananek T <info.dvgamer@gmail.com>
2018-03-11 13:09:06 +0700

    add new slack

--- notify/index.js
+++ notify/index.js
@@ -9,6 +9,8 @@ const apiLineID = process.env.LINE_ID
 const nameTouno = `https://hooks.slack.com/services/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`
 const nameException = `https://hooks.slack.com/services/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`
 const nameNotify = `https://hooks.slack.com/services/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`
+const nameGithub = `https://hooks.slack.com/services/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`
+const nameWakatime = `https://hooks.slack.com/services/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`
 const client = (token, msg) => {
   const cb = new api.Client({ channelAccessToken: token })
   return cb.pushMessage(apiLineID, msg)
@@ -45,6 +47,8 @@ module.exports = {
   Slack: {
     Notify: (msg, channel, name) => webhook(nameNotify, name, msg, channel),
     Exception: (msg, channel, name) => webhook(nameException, name, msg, channel),
-    Touno: (msg, channel, name) => webhook(nameTouno, name, msg, channel)
+    Touno: (msg, channel, name) => webhook(nameTouno, name, msg, channel),
+    Github: (msg, channel, name) => webhook(nameGithub, name, msg, channel),
+    Waka: (msg, channel, name) => webhook(nameWakatime, name, msg, channel)
   }
 }
