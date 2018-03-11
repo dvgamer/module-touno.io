d1f1fdb638d5442661415f0c11312a92c4add556
Kananek T <info.dvgamer@gmail.com>
2018-03-12 00:44:56 +0700

    fixed raven

--- helper/raven.js
+++ helper/raven.js
@@ -1,17 +1,10 @@
 const Raven = require('raven')
 const { debug } = require('./variables')
 
-if (process.env.RAVEN_CONFIG) {
-  Raven.config(process.env.RAVEN_CONFIG).install((err, initialErr, eventId) => {
-    console.error(err)
-    process.exit(1)
-  })
-}
-
+Raven.config(!debug && process.env.RAVEN_CONFIG).install((err, initialErr, eventId) => {
+  console.error(err)
+  process.exit(1)
+})
 module.exports = ex => {
-  if (!debug && process.env.RAVEN_CONFIG) {
-    Raven.captureException(ex)
-  } else {
-    console.log(`${ex.message || ex}`)
-  }
+  Raven.captureException(ex)
 }
