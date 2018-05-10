60708518e6667a08ccbfbd614fdead1a40479750
Kananek T <info.dvgamer@gmail.com>
2018-05-10 17:08:29 +0700

    Update raven.js

--- helper/raven.js
+++ helper/raven.js
@@ -1,10 +1,22 @@
 const Raven = require('raven')
-const { debug } = require('./variables')
+const debuger = require('./debuger')
 
 Raven.config(!debug && process.env.RAVEN_CONFIG).install((err, initialErr, eventId) => {
-  console.error(err)
+  debuger.error(err || initialErr)
   process.exit(1)
 })
-module.exports = ex => {
-  Raven.captureException(ex)
+
+let config = {}
+module.exports = {
+  set (data) {
+    config = Object.assign(config, data)
+  },
+  warning (ex) {
+    Raven.captureMessage(ex instanceof Error ? ex.message : ex, {
+      level: 'warning' // one of 'info', 'warning', or 'error'
+    })
+  },
+  error (ex) {
+    Raven.captureException(ex instanceof Error ? ex : new Error(ex), config)
+  }
 }
