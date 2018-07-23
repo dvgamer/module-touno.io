acca2df7b25ceeea59ca4aaaac5125a98ca817a6
Kananek T <info.dvgamer@gmail.com>
2018-07-23 15:39:35 +0700

    fixd version and add log color

--- helper/debuger.js
+++ helper/debuger.js
@@ -1,29 +1,39 @@
 const consola = require('consola')
 const { isDev } = require('./variables')
+const Youch = require('youch')
+const forTerminal = require('youch-terminal')
+const chalk = require('chalk')
 
+let scopeName = null; 
+let logger = consola
 module.exports = {
+  scope (name) {
+    scopeName = name
+    logger = name ? consola.withScope(scopeName) : logger = consola
+  },
   log (...msg) {
     if (!isDev) return
-    console.log(' ', ...msg)
+    console.log(chalk.gray('- debug'),`${scopeName ? `${scopeName} ${chalk.blue('»')}` : ''}`,...msg)
   },
   start (...msg) {
     if (!isDev) return
-    consola.start(msg.join(' '))
+    logger.start(msg.join(' '))
   },
   success (...msg) {
     if (!isDev) return
-    consola.success(msg.join(' '))
-    console.log()
+    logger.success(msg.join(' '))
   },
   info (...msg) {
     if (!isDev) return
-    consola.info(msg.join(' '))
+    logger.info(msg.join(' '))
   },
-  error (...msg) {
-    if (!isDev) return
-    console.log()
-    consola.error(msg.join(' '))
-    console.log()
+  async error (error) {
+    if (!isDev || !error) return
+    if (error instanceof Error) {
+      let output = await new Youch(error, {}).toJSON()
+      logger.error(error.message)
+      console.log(forTerminal(output))
+    }
   },
   progress: {
     begin (msg) {
--- mongodb/index.js
+++ mongodb/index.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose')
 mongoose.Promise = require('q').Promise
 
-const { isDev } = require('../helper/variables')
+const debuger = require('../helper/debuger')
 
 let db = []
 let mongodb = {
   MongooseOpen: async options => {
+    debuger.scope('MongoDB')
+    debuger.log(`Connection is 'mongodb://${login.user}@${MONGODB}/${MONGODBNAME}'.`)
     // { user: 'admin', pass: 'admin', dbname: 'test' }
     options = options || {}
-    const MONGODB = process.env.MONGODB || '127.0.0.1:27017'
+    const MONGODB = process.env.MONGODB || 'db-mongo:27017'
     const login = { user: options.user || process.env.MONGODB_USER, pass: options.pass || process.env.MONGODB_PASS }
-    const dbo = `mongodb://${MONGODB}/${options.dbname || process.env.MONGODB_NAME}${options.user ? '?authMode=scram-sha1?authSource=admin' : ''}`
-    if (isDev) console.log(`[MongoDB] ${mongoose.connection.readyState}:Connecting... 'mongodb://${MONGODB}/${options.dbname}'`)
+    const MONGODBNAME = options.dbname || process.env.MONGODB_NAME
+    const dbo = `mongodb://${MONGODB}/${MONGODBNAME}${login.user ? '?authMode=scram-sha1?authSource=admin' : ''}`
     await mongoose.connect(dbo, login)
+    debuger.log(`${mongoose.connection.readyState}: Connected.`)
   },
   tests: mongoose.model('db-tests', mongoose.Schema({ any: mongoose.Schema.Types.Mixed }), 'db-tests'),
   MongooseClose: async () => {
     await mongoose.connection.close()
-    if (isDev) console.log(`[MongoDB] Close connection.`)
+    debuger.scope('MongoDB')
+    debuger.log(`${mongoose.connection.readyState}: Closed.`)
   }
 }
 
