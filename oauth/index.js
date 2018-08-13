d1f27b1cae659750a29cc95e1814355a039cab44
Kananek T <info.dvgamer@gmail.com>
2018-08-14 00:46:19 +0700

    change name AccessToken

--- oauth/index.js
+++ oauth/index.js
@@ -3,7 +3,7 @@ const { isDev } = require('../helper/variables')
 const debuger = require('../helper/debuger')
 
 module.exports = {
-  Token: async auth => {
+  AccessToken: async auth => {
     let item = await OAuth.findOne({ name: auth })
     if (!item) {
       return { error: 'not oauth type, Please validate auth.', uri: `https://touno.io/auth/${auth}` }
