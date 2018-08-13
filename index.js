9d47f30590abcce2d7f7469850e553e2ee264351
Kananek T <info.dvgamer@gmail.com>
2018-08-13 15:04:43 +0700

    cadd close process

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ const TounoDB = require('./db-touno')
 const oAuth = require('./oauth')
 const Notify = require('./notify')
 
+const ProcessClose = afterCallBack => {
+  let abortProcess = async () => {
+    await afterCallBack()
+    process.exit(0)
+  }
+
+  process.on('SIGINT', () => abortProcess().catch(Raven))
+  process.on('SIGUSR1', () => abortProcess().catch(Raven))
+  process.on('SIGUSR2', () => abortProcess().catch(Raven))
+  process.on('uncaughtException', () => abortProcess().catch(Raven))
+}
+
 module.exports = {
-  oAuth: oAuth,
-  TounoDB: TounoDB,
-  Notify: Notify,
-  Time: Time,
-  debuger: debuger,
-  Raven: Raven
+  oAuth,
+  TounoDB,
+  Notify,
+  Time,
+  debuger,
+  Raven,
+  ProcessClose
 }
