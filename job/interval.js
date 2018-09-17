c1d3adf4b7ae04b4043c8fcce8b85d6354364819
Kananek T <info.dvgamer@gmail.com>
2018-09-17 14:46:20 +0700

    save setting intervel

--- job/interval.js
+++ job/interval.js
@@ -1,13 +1,13 @@
 const Raven = require('../helper/raven')
 
 module.exports = options => Raven.Tracking(async () => {
-  options = Object.assign({ once: false, second: 30, interval: 0 }, options)
+  options = Object.assign({ now: false, second: 30, interval: 0 }, options)
 
   let iLoop = 0
   let rawIntervel = 0
   const OnTickerEvent = async () => {
     iLoop++
-    if (!options.once) await Raven.Tracking(options.tick.bind(null, iLoop), true)
+    await Raven.Tracking(options.tick.bind(null, iLoop), true)
     await OnSetTimeoutEvent()
   }
   const OnSetTimeoutEvent = () => {
@@ -15,5 +15,5 @@ module.exports = options => Raven.Tracking(async () => {
     if (options.interval <= iLoop || !options.interval) rawIntervel = setTimeout(() => OnTickerEvent(), options.second * 1000)
   }
 
-  if (options.once) OnTickerEvent(); else OnSetTimeoutEvent()
+  if (options.now) OnTickerEvent(); else OnSetTimeoutEvent()
 })
