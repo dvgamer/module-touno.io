eb06630c70f63f9f5f583a71e36c662219db5dcc
Kananek T <info.dvgamer@gmail.com>
2018-09-20 19:59:14 +0700

    fixed oauth

--- oauth/index.js
+++ oauth/index.js
@@ -5,16 +5,19 @@ const AUTH_ENDPOINT = process.env.AUTH_ENDPOINT || `https://auth.touno.io`
 module.exports = {
   AccessToken: async sender => {
     if (!sender.auth) throw new Error(`AccessToken require 'auth' name.`)
-    if (!sender.uri) throw new Error(`AccessToken require 'uri' path.`)
+    if (!sender.uri && !sender.url) throw new Error(`AccessToken require 'uri' path.`)
 
-    let item = await request({ method: 'POST', uri: `${AUTH_ENDPOINT}/${sender.auth}/accesstoken`, json: true })
+    let item = await request({ method: 'GET', uri: `${AUTH_ENDPOINT}/${sender.auth}/accesstoken`, json: true })
+
     if (item.error) {
       throw new Error('Not oauth type, or please auth data from server.')
     } else {
       return request({
         method: sender.method || 'POST',
-        headers: Object.assign({ 'Authorization': `Bearer ${item.token.access_token}` }, sender.headers),
-        uri: sender.uri
+        headers: Object.assign({ 'Authorization': `Bearer ${item.access_token}` }, sender.headers),
+        resolveWithFullResponse: !!sender.resolveWithFullResponse,
+        uri: sender.uri || sender.url,
+        json: !!sender.json
       })
     }
   }
