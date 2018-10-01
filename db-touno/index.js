3dc6e2a10198f0df80d4b890c54b8caf6c5dcf22
TOUNO.io <info.dvgamer@gmail.com>
2018-10-01 15:32:56 +0700

    Update index.js

--- db-touno/index.js
+++ db-touno/index.js
@@ -8,12 +8,13 @@ module.exports = {
   open: async () => {
     if (!conn.connected()) {
       conn = await MongoConnection('db_touno', process.env.DBTOUNO_USER, process.env.DBTOUNO_SERVER)
+      MongoSchemaMapping(conn, require('./anime'))
       MongoSchemaMapping(conn, require('./app'))
+      MongoSchemaMapping(conn, require('./donate'))
+      MongoSchemaMapping(conn, require('./expanse'))
+      MongoSchemaMapping(conn, require('./github'))
       MongoSchemaMapping(conn, require('./schedule'))
       MongoSchemaMapping(conn, require('./wakatime'))
-      MongoSchemaMapping(conn, require('./github'))
-      MongoSchemaMapping(conn, require('./expanse'))
-      MongoSchemaMapping(conn, require('./donate'))
     }
     return conn
   }
