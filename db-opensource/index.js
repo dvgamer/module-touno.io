d7e86b04aa0097f66dcd1d115e7f297e08469c5b
TOUNO.io <info.dvgamer@gmail.com>
2018-10-01 15:27:19 +0700

    Update index.js

--- db-opensource/index.js
+++ db-opensource/index.js
@@ -10,6 +10,7 @@ module.exports = {
       conn = await MongoConnection('db_opensource', process.env.DBOPENSOURCE_USER, process.env.DBOPENSOURCE_SERVER)
       MongoSchemaMapping(conn, require('./exhentai'))
       MongoSchemaMapping(conn, require('./pokedex'))
+      MongoSchemaMapping(conn, require('./anilist'))
     }
     return conn
   }
